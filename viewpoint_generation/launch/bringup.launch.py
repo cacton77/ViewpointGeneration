e72f437725dad7daebb07ba9cb80c99965be8aa6
Colin Acton <actonc@uw.edu>
2025-08-08 13:07:11 -0700

    created launch files for full system bringup and viewpoint traversal bringup

---
+++ viewpoint_generation/launch/bringup.launch.py
@@ -0,0 +1,83 @@
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.conditions import IfCondition, UnlessCondition
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+from launch_ros.substitutions import FindPackageShare
+
+
+def generate_launch_description():
+    declared_arguments = [
+        DeclareLaunchArgument("sim", default_value="false",),
+        DeclareLaunchArgument("ur_type", default_value="ur5e"),
+        DeclareLaunchArgument("use_fake_hardware", default_value="true"),
+        DeclareLaunchArgument("mock_sensor_commands", default_value="false",
+                              description="Enable fake command interfaces for sensors used for simple simulations. "
+                              "Used only if 'use_fake_hardware' parameter is true."),
+        DeclareLaunchArgument("headless_mode", default_value="false",
+                              description="Run in headless mode (without GUI)."),
+        DeclareLaunchArgument("robot_ip", default_value="0.0.0.0",
+                              description="IP address of the robot."),
+        DeclareLaunchArgument("safety_limits", default_value="true",
+                              description="Enable safety limits controller."),
+        DeclareLaunchArgument("safety_pos_margin", default_value="0.15",
+                              description="Safety margin for position limits."),
+        DeclareLaunchArgument("safety_k_position", default_value="20",
+                              description="k-position factor in safety controller."),
+    ]
+
+    simulation_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([
+            PathJoinSubstitution([
+                FindPackageShare("inspection_cell_moveit_config"),
+                "launch",
+                "inspection_cell_sim.launch.py"
+            ])
+        ]),
+        condition=IfCondition(LaunchConfiguration("sim")),
+        launch_arguments={
+            "use_fake_hardware": LaunchConfiguration("use_fake_hardware"),
+            "mock_sensor_commands": LaunchConfiguration("mock_sensor_commands"),
+            "headless_mode": LaunchConfiguration("headless_mode"),
+            "robot_ip": LaunchConfiguration("robot_ip"),
+            "safety_limits": LaunchConfiguration("safety_limits"),
+            "safety_pos_margin": LaunchConfiguration("safety_pos_margin"),
+            "safety_k_position": LaunchConfiguration("safety_k_position")
+        }.items()
+    )
+
+    hardware_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([
+            PathJoinSubstitution([
+                FindPackageShare("inspection_cell_moveit_config"),
+                "launch",
+                "inspection_cell_hw.launch.py"
+            ])
+        ]),
+        condition=UnlessCondition(LaunchConfiguration("sim")),
+        launch_arguments={
+            "use_fake_hardware": LaunchConfiguration("use_fake_hardware"),
+            "mock_sensor_commands": LaunchConfiguration("mock_sensor_commands"),
+            "headless_mode": LaunchConfiguration("headless_mode"),
+            "robot_ip": LaunchConfiguration("robot_ip"),
+            "safety_limits": LaunchConfiguration("safety_limits"),
+            "safety_pos_margin": LaunchConfiguration("safety_pos_margin"),
+            "safety_k_position": LaunchConfiguration("safety_k_position")
+        }.items()
+    )
+
+    viewpoint_traversal_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([
+            PathJoinSubstitution([
+                FindPackageShare("viewpoint_generation"),
+                "launch",
+                "viewpoint_traversal.launch.py"
+            ])
+        ]),
+    )
+
+    return LaunchDescription(declared_arguments + [
+        simulation_launch,
+        hardware_launch,
+        viewpoint_traversal_launch
+    ])
---
+++ viewpoint_generation/launch/viewpoint_traversal.launch.py
@@ -0,0 +1,39 @@
+import time
+from launch import LaunchDescription
+from moveit_configs_utils import MoveItConfigsBuilder
+from moveit_configs_utils.launches import generate_move_group_launch
+from ur_moveit_config.launch_common import load_yaml
+from launch_ros.actions import Node
+from launch.actions import TimerAction
+
+
+def generate_launch_description():
+    moveit_config = (
+        MoveItConfigsBuilder(
+            "inspection_cell",
+            package_name="inspection_cell_moveit_config")
+        .robot_description_semantic(file_path="config/inspection_cell.srdf")
+        .robot_description_kinematics(file_path="config/kinematics.yaml")
+        .moveit_cpp(file_path="config/motion_planning.yaml")
+        .joint_limits(file_path="config/joint_limits.yaml")
+        .planning_pipelines(default_planning_pipeline="ompl", pipelines=["ompl", "chomp", "pilz_industrial_motion_planner"])
+        .trajectory_execution(moveit_manage_controllers=True)
+        .planning_scene_monitor(
+            publish_planning_scene=True,
+            publish_geometry_updates=True,
+            publish_state_updates=True,
+            publish_transforms_updates=True,
+        )
+        .to_moveit_configs()
+    )
+
+    # Your MoveItPy Viewpoint Traversal Node
+    viewpoint_traversal_node = Node(
+        name="viewpoint_traversal_node",
+        package="viewpoint_generation",
+        executable="viewpoint_traversal_node",
+        output="both",
+        parameters=[moveit_config.to_dict()],
+    )
+
+    return LaunchDescription([viewpoint_traversal_node])
